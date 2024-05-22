d835707329e6d0e6f620c81561382bcba92bc6b5
Nicolás Salerno <amplificodora04@gmail.com>
2024-05-22 01:06:07 -0300

    Prueba POST

--- src/app.ts
+++ src/app.ts
@@ -2,16 +2,17 @@ import express from 'express';
 import { Evento } from './evento.js';
 
 const app = express();
+app.use(express.json());
 
 const eventos = [
   new Evento(
-    'id-de-prueba-test-lorem_ipsum',
     'Wos en el luna park',
     'Es Wos!!! en el luna park!!!!',
     5000,
     'catamarca 1540',
-    '2023-05-20T15:30:00',
-    18
+    '2023-05-19',
+    18,
+    'id-de-prueba-test-lorem_ipsum'
   ),
 ];
 
@@ -20,9 +21,40 @@ const eventos = [
 //});
 
 app.get('/api/eventos', (req, res) => {
-  res.json(eventos);
+  res.json({ data: eventos });
+});
+
+app.get('/api/eventos/:id', (req, res) => {
+  const evento = eventos.find((evento) => evento.id === req.params.id);
+  if (!evento) {
+    res.status(404).send({ message: 'Evento no encontrado' });
+  }
+  res.json({ data: evento });
 });
 
 app.listen(3000, () => {
   console.log('Server running on http://localhost:3000/');
+});
+
+app.post('/api/eventos', (req, res) => {
+  const {
+    nombre,
+    descripcion,
+    capacidad_total,
+    direccion,
+    fecha_hora,
+    edad_minima,
+  } = req.body;
+
+  const evento = new Evento(
+    nombre,
+    descripcion,
+    capacidad_total,
+    direccion,
+    fecha_hora,
+    edad_minima
+  );
+
+  eventos.push(evento);
+  res.status(201).send({ message: 'Evento creado', data: evento });
 });
--- src/evento.ts
+++ src/evento.ts
@@ -2,12 +2,12 @@ import crypto from 'node:crypto';
 
 export class Evento {
   constructor(
-    public id = crypto.randomUUID(),
     public nombre: string,
     public descripcion: string,
     public capacidad_total: number,
     public direccion: string,
     public fecha_hora: string,
-    public edad_minima: number
+    public edad_minima: number,
+    public id = crypto.randomUUID()
   ) {}
 }
