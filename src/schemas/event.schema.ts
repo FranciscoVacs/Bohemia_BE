e157dc2ee4c5f5a25bde2acca67bf76a3219fd5e
FranciscoVacs <franciscovacs@gmail.com>
2024-11-05 17:40:49 -0300

    fechas evento schema

--- src/schemas/event.schema.ts
+++ src/schemas/event.schema.ts
@@ -1,6 +1,6 @@
 import { z } from "zod";
 
-const datetimeRegex = /^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}([-+]\d{2}:\d{2}|Z)$/;
+const datetimeRegex = /^\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}$/;
 
 export const CreateEventSchema = z.object({
   body: z.object({
