69ef5c15ae3a0018e8cd8ca30a3c9498e1b6f540
Nicolás Salerno <amplificodora04@gmail.com>
2026-02-09 10:09:29 -0300

    mercadopago adjustments

--- src/middlewares/cors.ts
+++ src/middlewares/cors.ts
@@ -10,15 +10,17 @@ const ACCEPTED_ORIGINS = [
 
 export const corsMiddleware = ({ acceptedOrigins = ACCEPTED_ORIGINS } = {}) =>
   cors({
-    origin: (origin, callback) => {
-      if (typeof origin === "string" && acceptedOrigins.includes(origin)) {
+    origin: (origin, callback) => {origin: true
+//      if (typeof origin === "string" && acceptedOrigins.includes(origin)) {
+//        return callback(null, true);
+//      }
+      if (
+        !origin ||
+        origin.startsWith("http://localhost") ||
+        origin.includes("ngrok-free.dev")
+      ) {
         return callback(null, true);
       }
-
-      if (!origin) {
-        return callback(null, true);
-      }
-
       return callback(new Error("Not allowed by CORS"));
     },
     exposedHeaders: ['token'],
